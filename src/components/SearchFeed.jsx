721b6914249e5eb8525ab44ca0af1f7c08a3b386
Bartek0074 <bartekdo123@gmail.com>
2022-12-04 23:52:40 +0100

    added SearchFeed

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Videos } from './index';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+import '../styles/SearchFeed.scss'
 
 export default function SearchFeed() {
-  return (
-    <div>SearchFeed</div>
-  )
+	const { searchTerm } = useParams();
+
+	const [videos, setVideos] = useState([]);
+
+	useEffect(() => {
+		fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) =>
+			setVideos(data.items)
+		);
+	}, [searchTerm]);
+
+	return (
+		<div className='searchFeed'>
+			<p className='searchFeed__title'>
+				Search results for <span>{searchTerm}</span> videos
+			</p>
+			<Videos videos={videos} />
+		</div>
+	);
 }
